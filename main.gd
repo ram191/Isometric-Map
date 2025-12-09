5b908437bc64c171f8b7101e4533785bbdeaaa73
RAM <ali_rayhan19@hotmail.com>
2025-12-09 14:31:19 +0700

    update

--- main.gd
+++ main.gd
@@ -1,12 +1,10 @@
 extends Node2D
+#
+#func _ready() -> void:
+	#await Auth.get_user_data()
+	#$Camera2D/Header/Panel/Username.text = Auth.username
+	#$Camera2D/Header/CoinHUD/CoinVal.text = str(Auth.balance)
 
-func _ready() -> void:
-	await Auth.get_user_data()
-	$Camera2D/Header/Panel/Username.text = Auth.username
-	$Camera2D/Header/CoinHUD/CoinVal.text = str(Auth.balance)
-	
-
-	
 func expand_menu() -> void:
 	var tween = create_tween()
 	tween.tween_property($Camera2D/Menu, "position:y", 0, 0.15)
